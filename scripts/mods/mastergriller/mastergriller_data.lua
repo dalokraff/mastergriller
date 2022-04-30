46342983ab63a218537140f167fda5dda80889be
dalokraff <87997987+dalokraff@users.noreply.github.com>
2022-04-30 17:16:25 -0500

    update workshop description

--- scripts/mods/mastergriller/mastergriller_data.lua
+++ scripts/mods/mastergriller/mastergriller_data.lua
@@ -2,7 +2,7 @@ local mod = get_mod("mastergriller")
 
 local menu = {
 	name = "Master Griller",
-	description = "Adds in a Master Griller's Toque Blanche and Golden Pan.\nGiving the option to replace Battlewizard's hat, Aqshy's Torchgate, with  the Toque Blanche and and Sienna's illusions for 1H Sword and Crowbill with the Golden Pan",
+	description = "We may never get the Songbook but now we at least have the Master Griller!\nThis mod adds in a Master Griller's Toque Blanche and Golden Pan. With the options to replace Battlewizard's hat, Aqshy's Torchgate, with  the Toque Blanche and and Sienna's \"Master's\" illusions for 1H Sword and Crowbill with the Golden Pan. This mod is client side so it is compatible with hosts or clients that do not have the mod.\n\nIf you don't already have the \"Master's Sword\" or \"Master's Crowbill\" illusions you can use [url=https://steamcommunity.com/sharedfiles/filedetails/?id=1425249043&searchtext=give+weapon]Give Weapon[/url] to make yourself a weapon with one of the skins.",
 	is_togglable = false,
 }
 
